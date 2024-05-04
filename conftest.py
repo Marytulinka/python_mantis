c3d784918223b935c4ebcf7b3a74003286c1125c
Mariya Tulina <mariya.tulina@nexign.com>
2024-05-04 22:51:42 +0400

    Удалённое управление по протоколу Telnet

--- conftest.py
+++ conftest.py
@@ -18,31 +18,31 @@ def load_config(file):
             target = json.load(f)
     return target
 
-@pytest.fixture
-def app(request):
-    global fixture
-    browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
-    login_config = load_config(request.config.getoption("--target"))['webadmin']
-    if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
-    fixture.session.ensure_login(username=login_config['username'], password=login_config['password'])
-    return fixture
-
-
-# @pytest.fixture(scope="session")
-# def config(request):
-#     return load_config(request.config.getoption("--target"))
-#
-#
 # @pytest.fixture
-# def app(request, config):
+# def app(request):
 #     global fixture
 #     browser = request.config.getoption("--browser")
+#     web_config = load_config(request.config.getoption("--target"))['web']
+#     login_config = load_config(request.config.getoption("--target"))['webadmin']
 #     if fixture is None or not fixture.is_valid():
-#         fixture = Application(browser=browser, config=config)
-#     fixture.session.ensure_login(username=config['webadmin']['username'], password=config['webadmin']['password'])
+#         fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+#     fixture.session.ensure_login(username=login_config['username'], password=login_config['password'])
 #     return fixture
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture
+def app(request, config):
+    global fixture
+    browser = request.config.getoption("--browser")
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, config=config)
+    fixture.session.ensure_login(username=config['webadmin']['username'], password=config['webadmin']['password'])
+    return fixture
 
 
 # @pytest.fixture(scope="session", autouse=True)
