dec0f02dc69b50cda80e3aec4f8023ddf9c4b529
Mariya Tulina <mariya.tulina@nexign.com>
2024-05-14 14:01:03 +0400

    Simple Object Access Protocol (SOAP): реализованы проверки для теста регистрации нового пользователя(из лекции), добавление нового проекта, удаление проекта

--- tests/test_add_project.py
+++ tests/test_add_project.py
@@ -1,13 +1,14 @@
 from model.project import Project
 
 
-def test_add_new_project(app, db):
-    old_project = db.get_project_list()
-    project = Project(name="first@_project")
+def test_add_new_project(app, config):
+    username = config['webadmin']['username']
+    password = config['webadmin']['password']
+    old_project = app.soap.mc_projects(username, password)
+    project = Project(name="project_name")
     if len(old_project) != 0:
         app.project.open_project_page()
         app.project.push_create_button()
     app.project.create_project(project)
-    new_project = db.get_project_list()
-    old_project.append(project)
-    assert sorted(old_project, key=Project.id_or_max) == sorted(new_project, key=Project.id_or_max)
+    new_project = app.soap.mc_projects(username, password)
+    assert len(old_project) + 1 == len(new_project)
--- tests/test_del_project.py
+++ tests/test_del_project.py
@@ -2,14 +2,15 @@ from model.project import Project
 import random
 
 
-def test_delete_project_by_id(app, db):
+def test_delete_project_by_id(app, config, db):
     if len(db.get_project_list()) == 0:
         app.project.create_project(Project(name="first_project"))
     old_project = db.get_project_list()
     project = random.choice(old_project)
     app.project.delete_project_by_id(project.id)
-    new_project = db.get_project_list()
+    username = config['webadmin']['username']
+    password = config['webadmin']['password']
+    new_project = app.soap.mc_projects(username, password)
     assert len(old_project) - 1 == len(new_project)
-    old_project.remove(project)
-    assert old_project == new_project
+
 
